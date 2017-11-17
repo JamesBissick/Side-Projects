33f84ea5234b5cc29f56075354dbc83f72ffa9ea
JamesHarrys <bissickj@gmail.com>
2017-11-17 01:33:37 +0100

    travail perso 17/11

--- OpenClassrooms/Intro to Javascript/Exercices/test.js
+++ OpenClassrooms/Intro to Javascript/Exercices/test.js
@@ -1,7 +1,83 @@
-console.log("start program");
+// ------- Else if
+
+/* var number = Number(prompt("Enter a number:"));
+if(number < 5) {
+    console.log(number + " is inferior to 5");
+}
+else if(number > 5) {
+    console.log(number + " is superior to 5");
+}
+else if()
+else {
+    console.log(number + " is 5");
+}; */
+
+// ------- Else
+
+/* var number = Number(prompt("Enter a number"));
+if((number >= 10) && (number <= 100)) {
+    console.log(number + " is between 10 and 100.");
+}
+else {
+    console.log(number + " is not between 10 and 100.");
+}; */
+
+// ------- While loop
+
+/* console.log("Start of program");
 var number = 1;
-while(number <= 5) {
+while(number <= 100) {
     console.log(number);
     number++;
-};
-console.log("end of program")
+}
+console.log("End of program"); */
+
+// ------- For loop
+
+/* - Version 1 Basic 
+console.log("Start of  program");
+var counter;  // --- Define a variable called counter 
+// -- Defining the var in the for loop is called initialization
+for(counter =1; counter <= 5; counter++) { 
+    console.log(counter);
+}
+console.log("End of program")*/
+
+/* - Version 2 Shorter 
+console.log("Start fo Program");
+for(var counter = 1; counter <= 5; counter++) {
+    console.log(counter);
+}
+console.log("End of Program"); */
+
+/* EXERCICE
+
+No more hiding from your alarm clock! You've decided you want your computer to keep you 
+updated on the time so you're never late again. A talking clock takes a 24-hour time and 
+translates it into words.
+
+Input Description
+An hour (0-23) followed by a colon followed by the minute (0-59).
+
+Output Description
+The time in words, using 12-hour format followed by am or pm.
+
+Sample Input data
+00:00
+01:30
+12:05
+14:01
+20:29
+21:00
+
+Sample Output data
+It's twelve am
+It's one thirty am
+It's twelve oh five pm
+It's two oh one pm
+It's eight twenty nine pm
+It's nine pm
+
+Extension challenges (optional)
+Use the audio clips found here to give your clock a voice. */
+
