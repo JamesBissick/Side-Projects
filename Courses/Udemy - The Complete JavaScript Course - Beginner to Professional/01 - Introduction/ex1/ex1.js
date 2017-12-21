831b2225783586c68255ab778b6ae12ad10b8648
JamesHarrys <bissickj@gmail.com>
2017-12-21 01:34:15 +0100

    Javascript
    21/12

---
+++ Courses/Udemy - The Complete JavaScript Course - Beginner to Professional/01 - Introduction/ex1/ex1.js
@@ -0,0 +1,14 @@
+/* alert("Hello");
+document.write("Welcome!!!");
+console.log("It worked!");
+
+var question = "What is your name?";
+var userName = prompt(question);
+console.log(userName);
+alert("Hello " + userName); */
+
+var a = prompt("Number #1");
+
+var b = prompt("Number #2");
+
+var result = Number(a) == Number(b) ? "True" : "I like trains";
---
+++ Courses/Udemy - The Complete JavaScript Course - Beginner to Professional/03 - Multiple Values/03.js
@@ -0,0 +1,6 @@
+var array = ["strings", 100, false]; // literal array
+
+var myArray = new Array("strings", 100, false);
+console.log(array);
+
+var favoriteThings = ["chocolate","girls","tv shows","having fun",7,"civilizations",11,]
