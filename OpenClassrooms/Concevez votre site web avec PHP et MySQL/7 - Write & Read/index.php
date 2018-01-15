d7a895914970656ad9bf4ff700e2e1f4e925accd
JamesHarrys <bissickj@gmail.com>
2018-01-15 17:32:00 +0100

    15/01
    Suite Open Classroom

---
+++ OpenClassrooms/Concevez votre site web avec PHP et MySQL/7 - Write & Read/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Document</title>
+    </head>
+    <body>
+        <?php
+        $monfichier = fopen('./files/fichier.txt', 'r+');
+        
+        $pages_vues = fgets($monfichier); // On lit la première ligne (nombre de pages vues)
+        $pages_vues += 1; // On augmente de 1 ce nombre de pages vues
+        fseek($monfichier, 0); // On remet le curseur au début du fichier
+        fputs($monfichier, $pages_vues); // On écrit le nouveau nombre de pages vues
+        
+        fclose($monfichier);
+        
+        echo '<p>Cette page a été vue ' . $pages_vues . ' fois !</p>';
+        ?>
+    </body>
+</html>
