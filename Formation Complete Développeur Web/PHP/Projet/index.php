e18c215f71f876a0125b31b5a078e2a4475a4316
JamesHarrys <bissickj@gmail.com>
2018-02-07 22:08:54 +0100

    07/02 {workplace}
    B-day training.

--- Formation Complete Développeur Web/PHP/Projet/index.php
+++ Formation Complete Développeur Web/PHP/Projet/index.php
@@ -8,6 +8,8 @@
     // Message de remerciement final, false par défault pour ne pas afficher le message
     $isSuccess = FALSE;
 
+    // Definir à qui on envoie un email
+    $emailTo = "bissickj@gmail.com";
 
     /* Elles prendront les valeurs donné par l'utilisateur via $_POST*/
     /* On veut que les valeurs entrées reste affiché pour l'utilisateur, on va se servir des values dans les inputs */
@@ -18,37 +20,50 @@
         $phone  = verifyInput($_POST['phone']);
         $message  = verifyInput($_POST['message']);
         $isSuccess = TRUE;
+        $emailText = "";
 
         // Vérification des informations avec empty()
         if (empty($firstname)) {
             $firstnameError = "Please enter your firstname.";
             $isSuccess = FALSE;
+        } else {
+            $emailText .= "FirstName: $firstname\n";
         }
         if (empty($lastname)) {
             $lastnameError = "Please enter your lastname.";
             $isSuccess = FALSE;
+        } else {
+            $emailText .= "Lastname: $lastname\n";
         }
-        if (empty($phone)) {
-            $phoneError = "Please enter your phone number.";
-            $isSuccess = FALSE;
-        }
-        if (empty($message)) {
-            $messageError = "Please enter your message.";
-            $isSuccess = FALSE;
-        }
+
         // If email isn't TRUE then emailError message
         if (!isEmail($email)) {
             $emailError = "This is not a valid email.";
             $isSuccess = FALSE;
+        } else {
+            $emailText .= "Email: $email\n";
         }
         // If phoneNumber isn't TRUE then phoneError message
         if (!isPhoneNumber($phone)) {
             $phoneError = "Enter your number with space.";
             $isSuccess = FALSE;
+        } else {
+            $emailText .= "Phone Number: $phone\n";
         }
+        if (empty($message)) {
+            $messageError = "Please enter your message.";
+            $isSuccess = FALSE;
+        } else {
+            $emailText .= "Message: $message\n";
+        }
+
+
         // Comfirmation envoie d'email
         if ($isSuccess) {
-            // Envoie de l'email
+            $headers = "From: $firstname $lastname <$email>\rnReply-To: $email";
+            mail($emailTo, "A message from your website", $emailText, $headers);
+            // Remettre tous les champs à "vide"
+            $firstname = $lastname = $email = $phone = $message = "";
         }
     }
     // Cas à part pour email
@@ -60,7 +75,7 @@
     function isPhoneNumber($var) {
         // Expressions régulières pour vérifier que le numéro est un tél valide
         // A noter l'espace aprés 9, qui signifie qu'on accepte les espaces
-        return preg_match("/^[0-9 ]*$/");
+        return preg_match("/^[0-9 ]*$/", $var);
     }
 
     // Nous allons aussi protéger les inputs $_POST, verifyInput()
@@ -127,10 +142,9 @@
                                 placeholder="Your phone number" value="<?php echo $phone; ?>">
                             </div>
                             <div class="col-md-12">
+                            <!-- required class="form-control" to force the user to fill this input -->
                                 <label for="message">Message<span class="blue"> *</span></label>
-                                <!-- required class="form-control" to force the user to fill this input -->
-                                <textarea id="message" name="message" required class="form-control" class="form-control" placeholder="Your message" rows="4">
-                                <?php echo $message ?></textarea>
+                                <textarea id="message" name="message" required class="form-control" class="form-control" placeholder="Your message" rows="4"><?php echo $message ?></textarea>
                                 <p class="comments"><?php echo $messageError ?></p>
                             </div>
                             
